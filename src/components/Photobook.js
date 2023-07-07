8f9f34b26f7072f57ffa70e5815c42ae7098f22f
Harsh Raj <78961911+Harshraj77@users.noreply.github.com>
2023-07-07 15:39:57 +0530

    mobile device compatibility

--- src/components/Photobook.js
+++ src/components/Photobook.js
@@ -2,30 +2,47 @@ import React, { useRef } from "react";
 import HTMLFlipBook from "react-pageflip";
 import { Box, Center, Image, VStack } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
-
+import { useMediaQuery } from "@chakra-ui/react";
 function Photobook(props) {
   //passing a refernce and then adding it to html5flipbook
-
+  const [isMobile] = useMediaQuery("(max-width: 768px)");
   const book = useRef();
 
   return ( 
     <Center mt={5}>
       <VStack spacing={0}>
-        <Box >
-          <HTMLFlipBook
-            width={400}
-            height={600}
+        <Box sx={{width:"80%"}}>
+          {/* mobile view code */}
+          {isMobile && <HTMLFlipBook
+            width={200}
+            height={400}
             drawShadow={false}
-            usePortrait={false}
-            showCover={true}
+            showCover={false}
             ref={book}
+            usePortrait ={true}
           >
             {props.imageData.map((id) => (
               <Box>
                 <Image key={id} src={id} />
               </Box>
             ))}
-          </HTMLFlipBook>
+          </HTMLFlipBook>}
+
+          {!isMobile && <HTMLFlipBook
+            width={400}
+            height={600}
+            drawShadow={false}
+            showCover={false}
+            ref={book}
+            usePortrait ={false}
+          >
+            {props.imageData.map((id) => (
+              <Box>
+                <Image key={id} src={id} />
+              </Box>
+            ))}
+          </HTMLFlipBook>}
+          
         </Box>
         <Box>
           <Button
